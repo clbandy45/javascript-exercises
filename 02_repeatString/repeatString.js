7e5ac94bb4630f3d41ab5039feba32eea46feae4
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-10 20:44:34 -0600

    01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,rpt) {
+    let result = "";
+    if (rpt < 0) {
+        result = "ERROR";
+    } else {
+        for (let i = 0; i < rpt; i++) {
+        result += str;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
