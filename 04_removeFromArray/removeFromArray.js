8bf9f25fec1b098be1a2ba905c9d99264204359e
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-11 17:17:26 -0600

    03 04 and start of 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rems) {
+    let result = Array.from(arr);
+    
+    for (const rem of rems) {
+        for (let i = 0; i < result.length; i++) {
+    const index = result.indexOf(rem);
+        if (index > -1){
+            result.splice(index,1);
+        }
+    }}
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+let result = 0;
+let max = Math.max(x,y);
+let min = Math.min(x,y);
 
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
