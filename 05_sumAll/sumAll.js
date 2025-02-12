3f92d4f2b656c88d8c2d66756139271b1b22ab84
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-12 16:29:26 -0600

    more work on js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,10 +3,15 @@ let result = 0;
 let max = Math.max(x,y);
 let min = Math.min(x,y);
 
+//isNaN is failing the non-numbers
+if ((x || y) < 0 || (x || y) % 1 !== 0 || isNaN(x || y)){
+    return 'ERROR';
+} else {
     for (let i = min; i <= max; i++) {
         result += i;
     }
     return result;
+    }
 };
 
 // Do not edit below this line
