d2097c0bd19008a6fb15cc351cdc09b8c5de8bcf
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-25 18:34:43 -0600

    08 and 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const alphaNumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleaned = palindrome.toLowerCase().split('').filter(char => alphaNumerical.includes(char)).join('');
+    const reversed = cleaned.split('').reverse().join('');
 
+    return cleaned === reversed;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
